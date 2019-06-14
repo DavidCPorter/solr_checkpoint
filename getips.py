3303bb319aa8bb89e308e3af3931f117504afb73
David Porter <david@claimdocs.com>
2019-06-14 18:10:26 -0500

    script auto-generates ansible inventory file for cluster

--- getips.py
+++ getips.py
@@ -3,6 +3,10 @@ import sys
 import paramiko
 import base64
 
+from jinja2 import Template
+
+
+
 if len(sys.argv) != 4:
     print("\nusage: getips.py <username> <clouddnsfile> <path_to_private_rsa_key>\n")
     exit()
@@ -14,16 +18,17 @@ node1=''
 node2=''
 node3=''
 
-print(sys.argv)
 with open(sys.argv[2], 'r') as domainfile:
     num=0
     for line in domainfile:
-        print('node'+str(num))
         exec('%s=%s'%('node'+str(num),"'"+str(line[:-1])+"'"))
         num+=1
-print(node0,'shit')
+
 k = paramiko.RSAKey.from_private_key_file(sys.argv[3])
 hosts=[node0,node1,node2,node3]
+host_file=[]
+tnode=''
+
 open('hostsIps','w').close()
 
 for host in hosts:
@@ -32,6 +37,20 @@ for host in hosts:
     ssh.connect(host,username=user,pkey=k)
     print ("Connected to %s" % host)
     stdin, stdout, stderr = ssh.exec_command("ifconfig | awk '/inet/ {print $2}' | head -n 1")
-    [open('hostsIps','a').write(line) for line in stdout]
     [print(line) for line in stderr]
+    if host == node3:
+        tnode=stdout.readlines().pop()
+        ssh.close()
+        break
+    host_file.append(stdout.readlines().pop())
     ssh.close()
+
+print("...generating inventory file with Ips -> ./inventory_gen.txt")
+with open('hostfile.j2') as file_:
+    template = Template(file_.read())
+template = template.render(hostsfile=host_file,traffic_node=tnode,host_user=user)
+
+with open("inventory_gen.txt", "w") as inv:
+    inv.write(template)
+
+exit()
